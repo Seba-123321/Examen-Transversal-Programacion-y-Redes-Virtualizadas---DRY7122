11a68a673b71440a9e9b1aab05d4c607c8c01558
sebastian <seba.gonzalezr@duocuc.cl>
2025-07-20 18:41:42 -0400

    integrantes.py

---
+++ integrantes.py
@@ -0,0 +1,8 @@
+# integrantes.py
+
+print("Integrantes del grupo:")
+
+integrantes = ["Sebastian Gonzalez", "Mauricio Moran", "Luciano Sanchez"]
+
+for persona in integrantes:
+    print("- " + persona)
