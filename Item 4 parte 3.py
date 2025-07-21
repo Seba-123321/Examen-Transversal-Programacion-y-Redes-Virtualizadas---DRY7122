4155d92c5807af37394976430d9dd39aca9435a6
sebastian <seba.gonzalezr@duocuc.cl>
2025-07-21 15:16:31 -0400

    Create Item 4 parte 3.py

---
+++ Item 4 parte 3.py
@@ -0,0 +1,41 @@
+import xml.dom.minidom
+from ncclient import manager
+
+m = manager.connect(
+    host="192.168.56.106",
+    port=830,
+    username="cisco",
+    password="cisco123!",
+    hostkey_verify=False
+    )
+
+netconf_filter = """
+<filter>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
+</filter>
+"""
+netconf_reply = m.get_config(source="running", filter=netconf_filter)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+
+netconf_loopback = """
+<config>
+ <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+  <interface>
+   <Loopback>
+    <name>11</name>
+    <ip>
+     <address>
+      <primary>
+       <address>11.11.11.11</address>
+       <mask>255.255.255.255</mask>
+      </primary>
+     </address>
+    </ip>
+   </Loopback>
+  </interface>
+ </native>
+</config>
+"""
+netconf_reply = m.edit_config(target="running", config=netconf_loopback)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
